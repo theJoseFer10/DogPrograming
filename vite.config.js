23c0f6dfc9280fb6f50852e334375a1ba3f4a7b0
José Fernando Arenas Camacho <jose.arenas8078@alumnos.udg.mx>
2026-03-30 23:53:58 -0600

    fix: bug deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/DogProgramming/',
   plugins: [
     react(),
     tailwindcss(),
